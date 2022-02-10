3712af377d9040e53bcab772d3a60ed332386d9f
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-02-10 13:49:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,16 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int min = 0, c = 1;;
+    if (a < b) {
+        min = a;
+    }
+    else {
+        min = b;
+    }
+    for (int i = 1;i<((min/2) + 1);i++) {
+        if ((a%i == 0) && (b%i == 0))
+            c = i;
+    }
+    return c;
 }
